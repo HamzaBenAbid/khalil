1fbf7a345a920ff6068f3d3895e6bbe8f350d4f0
hamza <hamzabozambo@gmail.com>
2021-06-09 18:51:11 +0200

    2end commit

--- app/src/main/java/com/example/myapplication/Desander.java
+++ app/src/main/java/com/example/myapplication/Desander.java
@@ -1,13 +1,26 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Desander extends AppCompatActivity {
-
+    Button frictional, mud;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desander);
+
+        frictional=findViewById(R.id.Frictional);
+        frictional.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent desi = new Intent(Desander.this, Frictional.class);
+                startActivity(desi);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/Mudclarner.java
+++ app/src/main/java/com/example/myapplication/Mudclarner.java
@@ -1,7 +1,14 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Mudclarner extends AppCompatActivity {
 
@@ -9,5 +16,5 @@ public class Mudclarner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mudclarner);
-    }
-}
+
+    }}

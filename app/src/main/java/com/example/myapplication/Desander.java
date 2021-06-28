055aaf5133a89a96efb6cb82e91eb659b5018738
hamza <hamzabozambo@gmail.com>
2021-06-28 17:01:34 +0200

    3end commit

--- app/src/main/java/com/example/myapplication/Desander.java
+++ app/src/main/java/com/example/myapplication/Desander.java
@@ -8,12 +8,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Desander extends AppCompatActivity {
-    Button frictional, mud;
+ Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desander);
-
+b1 =findViewById(R.id.prob1);
+b1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent shelli = new Intent(Desander.this, Motor_will_not_start.class);
+        startActivity(shelli);
+    }
+});
 
     }
 }
